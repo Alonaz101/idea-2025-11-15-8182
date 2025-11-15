9865bc12e5a7214e4d0c2d4a131fc47fd3edbde3
Alonaz101 <138678065+Alonaz101@users.noreply.github.com>
2025-11-15 08:06:37 +0200

    Add main React App with routing

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import RecipeDetail from './pages/RecipeDetail';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/recipe/:id' element={<RecipeDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
